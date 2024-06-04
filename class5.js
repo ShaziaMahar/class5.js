67dda427f5619b166d1803f18685a786e34177a0
ShaziaMahar <shaziamahar478@gmail.com>
2024-06-04 09:56:13 +0500

    commit5

---
+++ class5.js
@@ -0,0 +1,142 @@
+fuction
+// function greet(){
+//         console.log("hello student");
+// }
+//  greet() function calling
+// greet()
+// greet()
+// greet()
+// greet()
+// greet()
+
+
+// function employee ( file){ //parameter
+// console.log("working on file" + file);
+// }
+// employee(" shazia")   // argument
+// employee(" hadia") 
+// employee(" wajeeha") 
+// employee(" zoya") 
+// employee(" shazia") 
+
+
+// function employee ( file){ //parameter
+//         return "working on file" + file
+// }
+// let data = employee (7);
+// console.log( data);
+
+
+
+// function add (num1 , num2 ,num3){
+
+//         return num1 + num2 + num3;
+// }
+// let data = add(22, 22 , 33)
+// let data2 = add(222, 22, 333);
+// console.log(data , data2);
+
+
+//Recursion not
+
+// function writeData(data , times){
+//         // document.write(data)
+//         if(times > 0){
+//         document.write(data , times - 1)
+//         }
+// }
+// writeData("hello", 25 )
+
+
+
+
+
+
+//closure ==== scope
+// let abc = 123 // global scope
+//function print (){
+// let abc =  123;  // local scope
+//         console.log();
+// }
+// console.log(abc);
+// print()
+ 
+
+// let name = "shazia"
+// function print (){
+//         let nam2 = "hadia";  //global scope
+// // console.log(name + nam2);
+
+// return function (){ // anonymous function
+//         let nam2 = "shazia";   //local scope
+//         console.log(nam2);
+
+
+// }
+// }
+
+// function print(){
+//         let num = "sawera";
+//         console.log(num);
+//         return function(){
+//                 console.log(num);
+//         }
+// }
+
+//  innerfunction = print();
+// innerfunction()
+
+
+
+// function greet(){
+//         console.log("what is your name");
+// }
+// greet()
+// greet()
+// greet()
+// greet()
+// greet()
+// greet()
+// greet()
+
+
+
+// function employee (file){
+//         console.log("working file "+ file);
+// }]\// employee( "shazia")
+// employee( "fozia")
+// employee( "fozia")
+// employee( "fozia")
+// employee( "fozia")
+// employee( "fozia")
+// // global scope
+// let num = "shazia"
+// function print(){
+//         let num2 = "hadeesa"
+//         console.log(num + num2);
+// }
+// print()
+
+
+// // local scope
+// function print(){
+//         let num = "sawera"
+//         console.log(num);
+// }
+// // print()
+// let innerfuction = print()
+// innerfuction()
+// function print(){
+//         let num = "123";
+//         console.log(num);
+//         return function(){
+//                 console.log(num);
+//         }
+// let a = 33;
+// a = 77;
+// a = 88;
+// {
+//     let a = 22;
+   
+// }
+// console.log(a);
